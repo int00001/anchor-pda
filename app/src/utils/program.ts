ace8bea9e477ee731d70ac197c080bbb244ade2b
int <int_eth@proton.me>
2023-04-13 21:07:27 -0500

    fix(utils): load idl from correct file

--- app/src/utils/program.ts
+++ app/src/utils/program.ts
@@ -1,5 +1,5 @@
 import * as anchor from '@coral-xyz/anchor';
-import { IDL } from '../../../target/types/anchor_test';
+import { IDL } from '../../../target/types/anchor_pda';
 
 export const getProgram = (provider: anchor.Provider, programId: string) =>
   new anchor.Program(IDL, programId, provider);
