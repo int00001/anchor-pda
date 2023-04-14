180f311f3cb25df9e40a340ccbb42f21b445f746
int <int_eth@proton.me>
2023-04-13 20:38:38 -0500

    feat(programs): restaurant review program
    PDA program.
    Creates a PDA on call to post_review.
    Account is a Review which holds review data.

--- programs/anchor-pda/src/lib.rs
+++ programs/anchor-pda/src/lib.rs
@@ -1,15 +1,57 @@
 use anchor_lang::prelude::*;
 
-declare_id!("");
+declare_id!("3vzZJat5tfRWHDrfpSMeeWU7ZCFhewZm264ELinxDKkK");
 
 #[program]
 pub mod anchor_pda {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn post_review(
+        ctx: Context<ReviewAccounts>,
+        restaurant: String,
+        review: String,
+        rating: u8,
+    ) -> Result<()> {
+        let new_review = &mut ctx.accounts.review;
+        new_review.reviewer = ctx.accounts.signer.key();
+        new_review.restaurant = restaurant;
+        new_review.review = review;
+        new_review.rating = rating;
+        msg!(
+            "review for {} - {} stars",
+            new_review.restaurant,
+            new_review.rating
+        );
+        msg!("review: {}", new_review.review);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(restaurant: String, review: String)]
+pub struct ReviewAccounts<'info> {
+    // pass in review account
+    #[account(
+        init_if_needed, // create account if doesn't exist
+        payer = signer, // payer for account rent
+        space = 500,    // bytes for account
+        seeds = [restaurant.as_bytes().as_ref(), signer.key().as_ref()], // pda derive
+        bump, // pda derive
+    )]
+    pub review: Account<'info, Review>,
+    // signer is mutable (writable - sol debit to fund new account)
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    // system program will get called to create the new review account
+    pub system_program: Program<'info, System>,
+}
+
+// Review account definition
+// account will hold these 4 pieces of data
+#[account]
+pub struct Review {
+    pub reviewer: Pubkey,
+    pub restaurant: String,
+    pub review: String,
+    pub rating: u8,
+}
